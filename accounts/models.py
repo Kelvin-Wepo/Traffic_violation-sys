2e23a95229f95f9e045824d5f2cee4fa60ec809d
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-09 09:44:27 +0000

    created basic user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+# users/models.py
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+    # Add custom fields and methods here
+    pass
 # Create your models here.
