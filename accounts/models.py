1222a323ecf1cdfa0b9589e6cd5c6ce5552d5014
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-09 10:07:19 +0000

    added model fields phone, address

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,21 @@
 # users/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 class CustomUser(AbstractUser):
-    # Add custom fields and methods here
-    pass
-# Create your models here.
+    ROLES = (
+        ('user', 'User'),
+        ('administrator', 'Administrator'),
+        ('moderator', 'Moderator'),
+    )
+    
+    role = models.CharField(max_length=20, choices=ROLES, default='user')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    emergency_contact = models.CharField(max_length=100, blank=True)
+    license_plate_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.username
+
