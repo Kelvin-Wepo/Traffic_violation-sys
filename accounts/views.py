e316252d8e989274f953470b8c495aef08fddea2
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-10 13:46:30 +0000

    added login view and home view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,31 @@
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+
+from django.shortcuts import redirect
+
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to the 'home' URL after successful login
+            return redirect('home')  # Replace 'home' with the actual URL name for your home page
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
+
+
+def home(request):
+    return render(request, 'base.html')  # Replace 'login.html' with your login template path
