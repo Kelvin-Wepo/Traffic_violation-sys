1b5f50312181940aceb2ec534fdb3731274895fc
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-10 13:49:34 +0000

    Added templates directory, static file direct
    Removed secerts

--- TrafficViolationReport/settings.py
+++ TrafficViolationReport/settings.py
@@ -71,7 +71,9 @@ ROOT_URLCONF = 'TrafficViolationReport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # Path to your templates folder
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -162,7 +164,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL prefix for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/assets'),  # Path to your static files directory
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
