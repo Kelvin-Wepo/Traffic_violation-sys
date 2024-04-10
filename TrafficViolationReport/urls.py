ad23ecdc0d833f20c7ecdf32dfdfce45c99d40ca
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-04-10 13:50:50 +0000

    load static to home, login page

--- TrafficViolationReport/urls.py
+++ TrafficViolationReport/urls.py
@@ -19,18 +19,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from accounts.views import CustomUserViewSet
+from accounts.views import CustomUserViewSet, custom_login, home
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', custom_login, name='login'),
+    path('home/', home, name='home'),
     path('', include(router.urls)),
     path('accounts/', include('allauth.urls')),  # Allauth URLs
     path('dj-rest-auth/', include('dj_rest_auth.urls')),  # Django Rest Auth URLs
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),  # Registration URLs
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
