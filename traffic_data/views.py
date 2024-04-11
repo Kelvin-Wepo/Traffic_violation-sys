ecb6bb35a42e8519c133a80c8e73c7511aa268c7
kelvin wepo <kelvinwepo7710@gmail.com>
2024-04-11 06:32:42 +0300

    this commit modifies views.py

--- traffic_data/views.py
+++ traffic_data/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+from utils.mysql_utils import (
+    get_traffic_violation_markers,
+    get_traffic_violation_details,
+    search_traffic_violations,
+)
+# Backup mysql to bigquery
 
-# Create your views here.
+
+def search_traffic_violations_view(request):
+    keyword = request.GET.get('keyword', '')
+    time_range = request.GET.get('timeRange', 'all')
+    from_date = request.GET.get('fromDate', '')
+    to_date = request.GET.get('toDate', '')
+
+    data = search_traffic_violations(keyword, time_range, from_date, to_date)
+    return JsonResponse(data, safe=False)
+
+#  traffic_violation_markers_view
+def traffic_violation_markers_view(request):
+    data = get_traffic_violation_markers(request)
+    return JsonResponse(data, safe=False)
+
+# traffic_violation_details_view
+def traffic_violation_details_view(request, traffic_violation_id):
+    data = get_traffic_violation_details(request, traffic_violation_id)
+    return JsonResponse(data, safe=False)
+
+def home(request):
+    context = {'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY}
+    return render(request, 'traffic_data/home.html', context)
